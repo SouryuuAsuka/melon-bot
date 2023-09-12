02a7ff15526221ae77e6f0f9933d4dc21a389a6d
Asuka <asuka@>
2023-09-12 21:29:55 +0300

    giveMelon inc fix 4

--- src/use-case/giveMelon.use-cases.ts
+++ src/use-case/giveMelon.use-cases.ts
@@ -10,11 +10,8 @@ export const giveMelon = async (bot: TelegramBot, callbackQuery: any) => {
   const post = await Post.findOne({ keyboardId: msg.message_id })
   if(!post?.creatorId) throw new Error("Пост не найден")
   console.log("post.creatorId - "+post.creatorId)
-  const user = User.findByIdAndUpdate({ chatId: post.creatorId }, { $inc: { score: 1 } })
-  .catch((err)=>{
-    console.log(err)
-  })
-  console.log(user);
+  const user = await User.findByIdAndUpdate({ chatId: post.creatorId }, { $inc: { score: 1 } })
+  console.log(JSON.stringify(user));
   const text = `Админ передал тебе дыню🍈. Ты молодец!`;
   bot.sendMessage(post.creatorId, text);
   console.log("data.st - " + JSON.stringify(data.st))
