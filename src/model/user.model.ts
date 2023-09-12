63774a32df1784da613b53604df48b6891d1965b
Asuka <asuka@>
2023-09-12 22:22:41 +0300

    UserSchema lastname fix

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -2,9 +2,9 @@ import  mongoose, { Schema } from 'mongoose';
 import { IUser } from '../interface/IUser';
 
 const UserSchema = new Schema<IUser>({
-  username: { type: String, default: null, required: true },
-  firstname: { type: String, default: null, required: true },
-  lastname: { type: String, default: null, required: true },
+  username: { type: String, default: null, required: false },
+  firstname: { type: String, default: null, required: false },
+  lastname: { type: String, default: null, required: false },
   chatId: { type: Number, default: null, required: true },
   score: { type: Number, default: 0, required: true }
 });
