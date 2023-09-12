937d436aacd9e479d52c3acc364bfca2164f8962
Asuka <asuka@>
2023-09-12 19:05:57 +0300

    PostSchema fix 2

--- src/model/post.model.ts
+++ src/model/post.model.ts
@@ -1,12 +1,12 @@
-import  mongoose, { Schema } from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 import { IPost } from '../interface/IPost';
 
 const PostSchema = new Schema<IPost>({
-  creatorId: { type: Number, default: null },
-  postId: { type: Number, default: null },
-  keyboardId: { type: Number, default: null },
-  ownerId: { type: Number, default: null },
-  inChanelPostId: { type: Number, default: null},
+  creatorId: { type: Number, default: null, required: true },
+  postId: { type: Number, default: null, required: true },
+  keyboardId: { type: Number, default: null, required: true },
+  ownerId: { type: Number, default: null, required: true },
+  inChanelPostId: { type: Number, default: null, required: false },
 });
 
 export default mongoose.model<IPost>('Melon-Post', PostSchema);
