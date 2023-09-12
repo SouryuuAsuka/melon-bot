f220db5d4454559d8d2cd6df1b9fc4b9f5f3933a
Asuka <asuka@>
2023-09-12 19:42:20 +0300

    getMessage with forward

--- src/use-case/getMessage.use-cases.ts
+++ src/use-case/getMessage.use-cases.ts
@@ -8,21 +8,13 @@ const botOwner = process.env.BOT_OWNER as string;
 export const getMessage = async (bot: TelegramBot, msg: any) => {
   const chatId = msg.chat.id;
   //TODO: if(msg.from.id != botOwner)
-  const media = getMediaArray(msg);
-  let newMsg;
-  if (media.length > 0) {
-    newMsg = await bot.sendMediaGroup(botOwner, media);
-  } else {
-    const msgText = msg.text ?? '';
-    newMsg = await bot.sendMessage(botOwner, msgText);
-  }
-
+  const newPost = await bot.forwardMessage(botOwner, msg.chat.id, msg.message_id);
   const newKeyboard = await bot.sendMessage(botOwner,'Управление постом:',{
-    reply_to_message_id: newMsg.message_id,
+    reply_to_message_id: newPost.message_id,
     reply_markup: {
       inline_keyboard: generateKeyboard(msg.from.id)
     }
   })
-  await Post.create({creatorId: chatId, postId: newMsg.message_id, keyboardId: newKeyboard.message_id, ownerId: botOwner})
+  await Post.create({creatorId: chatId, postId: newPost.message_id, keyboardId: newKeyboard.message_id, ownerId: botOwner})
   return true;
 }
