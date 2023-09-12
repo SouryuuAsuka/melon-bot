51217687c2dc08c95697518322edd5d6e10e6a3e
Asuka <asuka@>
2023-09-12 19:06:25 +0300

    PostSchema fix 3

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -2,9 +2,9 @@ import  mongoose, { Schema } from 'mongoose';
 import { IUser } from '../interface/IUser';
 
 const UserSchema = new Schema<IUser>({
-  username: { type: String, default: null },
-  chatId: { type: Number, default: null },
-  score: { type: Number, default: null }
+  username: { type: String, default: null, required: true },
+  chatId: { type: Number, default: null, required: true },
+  score: { type: Number, default: 0, required: true }
 });
 
 export default mongoose.model<IUser>('Melon-User', UserSchema);
